edcb17ad0b77aead0961893716cd634bfc47e50b
Kochari <koceri.agalarov@gmail.com>
2020-02-10 19:12:50 +0400

    Summing task

--- src/main/java/app/HelloWorldApp.java
+++ src/main/java/app/HelloWorldApp.java
@@ -1,9 +1,19 @@
 package app;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class HelloWorldApp {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Input number");
+    double number=sc.nextDouble();
+    System.out.println("Input number2");
+    double number2=sc.nextDouble();
+    number=number+number2;
+    System.out.println(" Sum of numbers = "+ number);
   }
 
 }
